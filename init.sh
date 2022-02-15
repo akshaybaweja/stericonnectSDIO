ea8d2b6c0d2ce1043939e98db0d037e0699b4d43
Akshay <akshaybaweja.1996@gmail.com>
2022-02-14 20:43:36 -0500

    added newline before echoing in config.txt

--- init.sh
+++ init.sh
@@ -20,7 +20,7 @@ sudo systemctl enable stericonnectlogs.service
 sudo systemctl enable stericonnect.service
 echo "Service and Routine Setup Complete"
 
-sudo echo "dtoverlay=sdio,poll_once=off" >> /boot/config.txt
+sudo echo -en "\n\ndtoverlay=sdio,poll_once=off" >> /boot/config.txt
 
 echo "Rebooting Raspberry Pi..."
 sleep 1 && sudo reboot
